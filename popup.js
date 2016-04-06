fec742b42a82dfc666a75c5d580b536671bb01ca
Eliseo Ocampos <roskoff@gmail.com>
2016-04-06 17:40:09 -0400

    Limpieza de código
    Eliminación de porciones de código no utilizadas
    
    Signed-off-by: Eliseo Ocampos <roskoff@gmail.com>

--- popup.js
+++ popup.js
@@ -4,12 +4,6 @@ function render(element, o) {
     if (div) {
         div.innerHTML = o;
 
-        // Eliminamos el link para descargar la extensión
-        if (element == 'titulo') {
-            var spans = div.getElementsByTagName('span');
-            spans[0].innerHTML = '';
-        }
-
         //En caso que los partidos no estén cargados, agregar una imagen en blanco
         //para evitar error del tipo:
         //  Resource interpreted as Image but transferred with MIME type text/plain
@@ -38,22 +32,7 @@ function renderMain(o) {
     render("main", o);
 };
 
-function renderTitulo(o) {
-    render("titulo", o);
-};
-
 function fetchData(){
-    // Actualmente es innecesario obtener el titulo, ya que el numero de la fecha que se disputará
-    // viene como encabezado de la tabla que contiene los datos del siguiente partido.
-/*
-    var xhrTitulo = new XMLHttpRequest();
-    xhrTitulo.open('GET', 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22http%3A%2F%2Fwww.franjeado.com%2Fstats.php%22%20and%20xpath%3D%22%2Fhtml%2Fbody%2Fdiv%2Ftable%2Ftbody%2Ftr%5B2%5D%2Ftd%2Ftable%2Ftbody%2Ftr%2Ftd%2Ftable%5B1%5D%2Ftbody%2Ftr%2Ftd%5B2%5D%2Fp%22', true);
-    xhrTitulo.onload = function(e) {
-        renderTitulo(this.response);
-    };
-    xhrTitulo.send();
-*/
-
     var xhr = new XMLHttpRequest();
     xhr.open('GET', 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22http%3A%2F%2Fwww.franjeado.com%2Fstats.php%22%20and%20xpath%3D%27%2F%2Fhtml%2Fbody%2Fdiv%2Ftable%2Ftbody%2Ftr%5B2%5D%2Ftd%2Ftable%2Ftbody%2Ftr%2Ftd%2Ftable%2Ftbody%2Ftr%2Ftd%5B2%5D%2Fdiv%2Ftable%27', true);
     xhr.onload = function(e) {
